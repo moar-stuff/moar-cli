7eb3c4c0ac815b27945876a23e5f3b2628e66e45
Mark Farnsworth <farnsm1@labcorp.com>
2019-08-11 18:40:33 -0400

    Add notes for release command

--- src/moarCli/ReleaseCommand.ts
+++ src/moarCli/ReleaseCommand.ts
@@ -23,15 +23,18 @@ export class ReleaseCommand extends PackageCommand {
 
   protected async doRun(): Promise<void> {
     const args = process.argv
-    const patchAddOpt = this.options.finish
+    const finishOpt = this.options.finish
+    const commentTransform = this.theme.commentTransform
+    const commandTransform = this.theme.commandTransform
     for (let i = 3; i < args.length; i++) {
       const arg = args[i]
-      if (CliElement.match(patchAddOpt, arg)) {
-        this.log('# Use following to finish the release')
+      if (CliElement.match(finishOpt, arg)) {
+        this.log(`${commentTransform('# Use with pipe')} ${commandTransform('moar release --finish | sh')}`)
         this.log('echo "git flow release finish `moar at`" | pbcopy')
         return
       }
     }
+    this.log(`${commentTransform('# Use with pipe')} ${commandTransform('moar release | sh')}`)
     this.log('git flow release start `moar at -m`')
     this.log('npm version minor')
   }
